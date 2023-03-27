f9e6146fb089b6ad1727f971a25576ed8f85203f
Paulo Alves <106793350+Paulojr01@users.noreply.github.com>
2023-03-27 18:49:09 -0300

    ok

--- Api_filmes/Main.java
+++ Api_filmes/Main.java
@@ -1,4 +1,19 @@
 package Api_filmes;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class Main {
+    public static void main(String[] args) throws Exception{
+
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request =  HttpRequest.newBuilder(endereco).GET().build();
+        var response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+    }
 }
