97f7fe92abbdbd1b12170f32e3dea2e614bfe12c
Paulo Alves <106793350+Paulojr01@users.noreply.github.com>
2023-03-27 19:38:19 -0300

    Consumindo API
    Criado a conexão e a solicitação na Main

--- Api_filmes/Main.java
+++ Api_filmes/Main.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws Exception{
@@ -14,6 +16,20 @@ public class Main {
         var request =  HttpRequest.newBuilder(endereco).GET().build();
         var response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
+
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        for(Map<String, String> filme : listaDeFilmes){
+
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+
+        }
+
     }
+
+
 }
