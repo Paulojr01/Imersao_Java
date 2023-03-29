c4ac7044f69ec926d1b2c7ea865acea33a0402dd
Paulo Alves <106793350+Paulojr01@users.noreply.github.com>
2023-03-29 19:50:50 -0300

    ok

---
+++ Api_filmes/GeradorDeFigurinhas.java
@@ -0,0 +1,37 @@
+package Api_filmes;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+
+public class GeradorDeFigurinhas {
+
+
+    public void cria(InputStream inputStream, String nomeArquivo) throws Exception {
+        // Leitura da imagem
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
+        // Criar nova imagem em memória com transparência e com tamanho novo
+        int largura = imagemOriginal.getWidth();
+        int altura = imagemOriginal.getHeight();
+        int novaAltura = altura + 200;
+        BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
+        //Copiar a imagem original para nova imagem (em memória)
+        Graphics2D graphics = (Graphics2D)novaImagem.getGraphics();
+        graphics.drawImage(imagemOriginal, 0, 0, null);
+
+        // Configurar a fonte
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 400);
+        graphics.setColor(Color.yellow);
+        graphics.setFont(fonte);
+
+        // escrever uma frase na nova imagem
+        graphics.drawString("Legal", 500, novaAltura - 90);
+
+        //escrever a nova imagem em um arquivo
+        ImageIO.write(novaImagem, "png", new File(nomeArquivo));
+    }
+
+
+}
--- Api_filmes/Main.java
+++ Api_filmes/Main.java
@@ -1,11 +1,15 @@
 package Api_filmes;
 
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
+import java.io.InputStream;
+
 
 public class Main {
     public static void main(String[] args) throws Exception{
@@ -22,11 +26,21 @@ public class Main {
 
         for(Map<String, String> filme : listaDeFilmes){
 
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
-            System.out.println();
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+            InputStream inputStream = new URL(urlImagem).openStream();
 
+            String nomeArquivo = titulo + ".png";
+
+            var geradora = new GeradorDeFigurinhas();
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(filme.get(titulo));
+            System.out.println();
+            // System.out.println(filme.get("title"));
+            // System.out.println(filme.get("image"));
+            // System.out.println(filme.get("imDbRating"));
+            // System.out.println();
         }
 
     }
