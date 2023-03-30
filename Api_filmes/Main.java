a520783c0e040cf48dc9bec2373e180ec3b1f7be
Paulo Alves <106793350+Paulojr01@users.noreply.github.com>
2023-03-30 01:03:07 -0300

    consumindo api nasa

--- Api_filmes/Main.java
+++ Api_filmes/Main.java
@@ -8,13 +8,13 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
-import java.io.InputStream;
+
 
 
 public class Main {
     public static void main(String[] args) throws Exception{
 
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-16";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request =  HttpRequest.newBuilder(endereco).GET().build();
@@ -26,21 +26,17 @@ public class Main {
 
         for(Map<String, String> filme : listaDeFilmes){
 
-            String urlImagem = filme.get("image");
+            String urlImagem = filme.get("url");
             String titulo = filme.get("title");
             InputStream inputStream = new URL(urlImagem).openStream();
-
-            String nomeArquivo = titulo + ".png";
+            String nomeArquivo = "saida/" + titulo + ".png";
 
             var geradora = new GeradorDeFigurinhas();
             geradora.cria(inputStream, nomeArquivo);
 
-            System.out.println(filme.get(titulo));
+            System.out.println(titulo);
             System.out.println();
-            // System.out.println(filme.get("title"));
-            // System.out.println(filme.get("image"));
-            // System.out.println(filme.get("imDbRating"));
-            // System.out.println();
+
         }
 
     }
